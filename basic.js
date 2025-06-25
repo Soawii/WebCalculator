e3454cf7b29e2a2695d68134684bc175b5266e66
Soawii <65302714+Soawii@users.noreply.github.com>
2025-06-26 01:32:45 +0300

    Added complex calculator version that can handle expressions

--- basic.js
+++ basic.js
@@ -3,47 +3,9 @@ const ERROR_MSG = "NO NO NO NO NO NO NO NO NO NO NO NO NO";
 let state = "left"; // [left, right]
 let bar_top = document.querySelector(".calculator-bar-top");
 let bar_bottom = document.querySelector(".calculator-bar-bottom");
-let selectionPointer = [0, 0];
-let prevText = "";
-
-function updatePointer(newSelectionPointer) {
-    selectionPointer = newSelectionPointer;
-    bar_bottom.setSelectionRange(selectionPointer[0], selectionPointer[1]);
-    bar_bottom.focus();
-}   
 
 function isStringGood(string) {
     return !isNaN(Number(string)) && isFinite(Number(string));
-}
-
-function addStringAt(string, pointer) {
-    const newValue = bar_bottom.value.slice(0, pointer[0]) + string + bar_bottom.value.slice(pointer[1]);
-    if (!isStringGood(newValue)) {
-        updatePointer([bar_bottom.value.length, bar_bottom.value.length]);
-        return false;
-    }
-    bar_bottom.value = newValue;
-    updatePointer([pointer[0] + string.length, pointer[0] + string.length]);
-    return true;
-}
-
-function deleteStringAt(pointer) {
-    if (pointer[0] < pointer[1]) {
-        addStringAt('', pointer);
-    }
-    else if (pointer[0] > 0) {
-        bar_bottom.value = bar_bottom.value.slice(0, pointer[0] - 1) + bar_bottom.value.slice(pointer[0]);
-        updatePointer([pointer[0] - 1, pointer[0] - 1]);
-    }
-    else if (bar_top.textContent != '0' && bar_bottom.value == '') {
-        state = "left";
-        addStringAt(bar_top.textContent.split(' ')[0], [0, bar_bottom.value.length]);
-        bar_top.textContent = '0';
-    }
-}
-
-function clearString() {
-    deleteStringAt([0, bar_bottom.value.length]);
 }
 
 function isScientific(num) {
@@ -51,12 +13,12 @@ function isScientific(num) {
 }
 
 function performUnaryOperation(operator, a) {
-    let func = operator_to_func[operator];
+    let func = operator_to_function[operator];
     return func(a);
 }
 
 function performBinaryOperation(operator, a, b) {
-    let func = operator_to_func[operator];
+    let func = operator_to_function[operator];
     return func(a, b);
 }
 
@@ -75,12 +37,15 @@ function getNumberReadyForDisplay(number) {
     while (string.slice(-1) == '0') {
         string = string.slice(0,-1);
     }
+    while (string.slice(-1) == '0') {
+        string = string.slice(0,-1);
+    }
     if (string.slice(-1) == '.')
         string = string.slice(0, -1);
     return string;
 }
 
-let operator_to_func = {
+let operator_to_function = {
     // binary operators
     '+': (a, b) => a + b,
     '-': (a, b) => a - b,
@@ -93,24 +58,38 @@ let operator_to_func = {
 
     // other operators
     '.': () => {
-        addStringAt('.', selectionPointer);
+        if (!bar_bottom.textContent.includes('.'))
+            bar_bottom.textContent += '.';
     },
     '=': () => {
         if (state != "right")
             return;
-        if (!isStringGood(bar_bottom.value))
+        if (!isStringGood(bar_bottom.textContent))
             return;
-        let number = performBinaryOperation(getOperator(), getNumber(), Number(bar_bottom.value));
-        if (addStringAt(getNumberReadyForDisplay(number), [0, bar_bottom.value.length])) {
-            bar_top.textContent = "0";
-            state = "left";
+        let number = performBinaryOperation(getOperator(), getNumber(), Number(bar_bottom.textContent));
+        if (!isStringGood(String(number))) {
+            alert(ERROR_MSG);
+            return;
         }
+        bar_bottom.textContent = getNumberReadyForDisplay(number);
+        bar_top.textContent = "0";
+        state = "left";
     },
     '⌫': () => {
-        deleteStringAt(selectionPointer);
+        if (bar_bottom.textContent.length > 1) {
+            bar_bottom.textContent = bar_bottom.textContent.slice(0, -1);
+            return;
+        }
+        if (bar_top.textContent != '0' && bar_bottom.textContent == '0') {
+            state = "left";
+            bar_bottom.textContent = bar_top.textContent.split(' ')[0];
+            bar_top.textContent = '0';
+            return;
+        }
+        bar_bottom.textContent = '0';
     },
-    'C': () => {
-        clearString();
+    'CE': () => {
+        bar_bottom.textContent = '0';
         bar_top.textContent = '0';
         state = "left";
     },
@@ -126,113 +105,114 @@ let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let operators = [];
 let all_buttons = document.querySelectorAll("button");
 let id_to_button = {};
-
 id_to_button["Enter"] = document.querySelector("#NOSHIFT-Equal");
 
 for (let i = 0; i < all_buttons.length; i++) {
     id_to_button[all_buttons[i].id] = all_buttons[i];
-    if (all_buttons[i].textContent in operator_to_func)
-        operators.push(new Operator(all_buttons[i].textContent, all_buttons[i], operator_to_func[all_buttons[i].textContent]));
+    if (all_buttons[i].textContent in operator_to_function)
+        operators.push(new Operator(all_buttons[i].textContent, all_buttons[i], operator_to_function[all_buttons[i].textContent]));
 }
 
 let argumentNumberToOperators = Array.of([], [], []);
+
 for (let i = 0; i < operators.length; i++) {
     argumentNumberToOperators[operators[i].func.length].push(operators[i].name);
 }
 
-function processButtonPress(buttonText) {
+console.log(argumentNumberToOperators);
+
+document.querySelector(".calculator-input").addEventListener("click", (e) => {
+    if (!e.target.classList.contains("calculator-input-button"))
+        return;
+
+    let buttonText = e.target.textContent;
+
     if (numbers.includes(buttonText)) {
-        if (bar_bottom.value == '') {
-            addStringAt(buttonText, [0, 0]);
-        }
-        else {
-            addStringAt(buttonText, selectionPointer);
-        }
+        if (bar_bottom.textContent == '0')
+            bar_bottom.textContent = buttonText;
+        else 
+            bar_bottom.textContent += buttonText
     }
     else if (argumentNumberToOperators[2].includes(buttonText)) { // binary operators
-        if (!isStringGood(bar_bottom.value))
+        if (!isStringGood(bar_bottom.textContent))
             return;
         if (state == "right") {
-            if (bar_bottom.value == '') {
+            if (bar_bottom.textContent == '0') {
                 bar_top.textContent = getNumber() + " " + buttonText;
                 return;
             }
-            let number = performBinaryOperation(getOperator(), getNumber(), Number(bar_bottom.value));
-            addStringAt(getNumberReadyForDisplay(number), [0, bar_bottom.value.length]);
+            let number = performBinaryOperation(getOperator(), getNumber(), Number(bar_bottom.textContent));
+            if (!isStringGood(String(number))) {
+                alert(ERROR_MSG);
+                return;
+            }
+            bar_bottom.textContent = getNumberReadyForDisplay(number);
             bar_top.textContent = '0';
             state = "left";
         }
         if (state == "left") {
-            bar_top.textContent = bar_bottom.value + " " + buttonText;
-            clearString();
+            bar_top.textContent = bar_bottom.textContent + " " + buttonText;
+            bar_bottom.textContent = '0';
             state = "right";
         }
     }
     else if (argumentNumberToOperators[1].includes(buttonText)) { // unary operators
-        if (!isStringGood(bar_bottom.value))
+        if (!isStringGood(bar_bottom.textContent))
             return;
         if (state == "left") {
-            let number = performUnaryOperation(buttonText, Number(bar_bottom.value));
-            addStringAt(getNumberReadyForDisplay(number), [0, bar_bottom.value.length]);
+            let number = performUnaryOperation(buttonText, Number(bar_bottom.textContent));
+            if (!isStringGood(String(number))) {
+                alert(ERROR_MSG);
+                return;
+            }
+            bar_bottom.textContent = getNumberReadyForDisplay(number);
         }
         else if (state == "right") {
-            let number = performBinaryOperation(getOperator(), getNumber(), Number(bar_bottom.value));
+            let number = performBinaryOperation(getOperator(), getNumber(), Number(bar_bottom.textContent));
             if (!isStringGood(String(number))) {
                 alert(ERROR_MSG);
                 return;
             }
             bar_top.textContent = '0';
-            addStringAt(getNumberReadyForDisplay(performUnaryOperation(buttonText, number)), [0, bar_bottom.value.length]);
+            bar_bottom.textContent = getNumberReadyForDisplay(performUnaryOperation(buttonText, number));
             state = "left";
         }
     }
     else if (argumentNumberToOperators[0].includes(buttonText)) { // other operators
-        operator_to_func[buttonText]();
+        operator_to_function[buttonText]();
     }
-}
-
-document.querySelector(".calculator-input").addEventListener("click", (e) => {
-    if (!e.target.classList.contains("calculator-input-button"))
-        return;
-    processButtonPress(e.target.textContent);
-    prevText = bar_bottom.value;
-});
-
-bar_bottom.addEventListener("selectionchange", (e) => {
-    selectionPointer = [bar_bottom.selectionStart, bar_bottom.selectionEnd];
-});
-
-const allowedRegex = /^[0-9]*(\.[0-9]*)?$/;
-bar_bottom.addEventListener("input", (e) => {
-    if (!bar_bottom.value.match(allowedRegex)) {
-        bar_bottom.value = prevText;
-    }
-    prevText = bar_bottom.value;
-});
-
-bar_bottom.addEventListener("keydown", (e) => {
-    if (e.code == "Backspace" && bar_bottom.value == '' && bar_top.textContent != '0') {
-        processButtonPress('⌫');
-        e.preventDefault();
+    else {
+        console.log(`Unknown operator: ${buttonText}`);
     }
 });
+
 
 window.addEventListener("keydown", (e) => {
     if (e.code in id_to_button) {
-        id_to_button[e.code].click();
+        id_to_button[e.code].focus();
+        if (e.code != "Enter")
+            id_to_button[e.code].click();
     }
     else {
         if (e.shiftKey) {
             if (("SHIFT-" + e.code) in id_to_button) {
                 id_to_button["SHIFT-" + e.code].click();
+                id_to_button["SHIFT-" + e.code].focus();
             }
         }
         else {
             if (("NOSHIFT-" + e.code) in id_to_button) {
                 id_to_button["NOSHIFT-" + e.code].click();
+                id_to_button["NOSHIFT-" + e.code].focus();
             }
         }
     }
 });
 
-bar_bottom.focus();
+window.addEventListener('paste', (e) => {
+    const clipboardData = e.clipboardData || window.clipboardData; 
+    const pastedText = clipboardData.getData('text');
+    if (!isStringGood(pastedText))
+        return;
+    bar_bottom.textContent = pastedText;
+});
